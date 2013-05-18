2ea7795e7c0962aadb4802a1a0c4e466803d296d
Grissiom <chaos.proton@gmail.com>
2013-05-18 23:33:55 +0800

    refactor

--- sdo.py
+++ sdo.py
@@ -1,44 +1,7 @@
-import serial
-
 dev = None
 logf = None
 
 _procs = []
-
-def sdo_write(data):
-    if dev is not None:
-        dev.write(data)
-    return True
-
-def sdo_wait_line(target):
-    line = ''
-    while True:
-        c = dev.read(1)
-        if logf is not None:
-            logf.write(c)
-        if c == '\n':
-            if line[-1] == '\r':
-                line = line[:-1]
-            if line == target:
-                return True
-            else:
-                line = ''
-        else:
-            line += c
-        #print "cmp: (%s) vs (%s)" % (repr(line), repr(target))
-    return False
-
-def sdo_success(message):
-    print '======='
-    print  message
-    print '======='
-    return True
-
-def sdo_fail(message):
-    print '*******'
-    print  message
-    print '*******'
-    return False
 
 def loop(loop_time):
     for i in xrange(loop_time):
@@ -53,17 +16,51 @@ class proc(object):
         _procs.append(self)
         self._step = []
 
+    def _expose_closure(func):
+        def _func(self, data):
+            self._step.append(lambda : func(self, data))
+        return _func
+
+    @_expose_closure
     def write(self, data):
-        self._step.append(lambda p: sdo_write(data))
+        if dev is not None:
+            dev.write(data)
+        return True
 
-    def wait_line(self, data):
-        self._step.append(lambda p: sdo_wait_line(data))
+    @_expose_closure
+    def wait_line(self, target):
+        line = ''
+        while True:
+            c = dev.read(1)
+            if logf is not None:
+                logf.write(c)
+            if c == '\n':
+                if line[-1] == '\r':
+                    line = line[:-1]
+                if line == target:
+                    return True
+                else:
+                    line = ''
+            else:
+                line += c
+        return False
 
-    def success(self, data):
-        self._step.append(lambda p: sdo_success(data))
+    @_expose_closure
+    def success(self, message):
+        print '======='
+        print  message
+        print '======='
+        return True
+
+    @_expose_closure
+    def fail(self, message):
+        print '*******'
+        print  message
+        print '*******'
+        return False
 
     def __call__(self):
         for s in self._step:
-            if not s(self):
+            if not s():
                 return False
         return True
